095f0e3e25b2f362edcabea19d2ee0e0bf51461a
weiwenyu <2454150875@qq.com>
2023-12-01 17:30:44 +0800

    feat: 提交部分流程

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,87 @@
 import * as vscode from 'vscode'
-import cp from 'child_process'
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('valorantsound.showView', () => {
-        console.log(context.extensionUri)
+    const provider = new ValorantSoundViewProvider(context.extensionUri)
 
-        let audioFilePath = vscode.Uri.joinPath(context.extensionUri, 'src', 'audio', 'audio.mp3').fsPath
-        // 现在你可以使用这个路径来播放你的音频文件
-        cp.exec()
-    })
-
-    context.subscriptions.push(disposable)
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(ValorantSoundViewProvider.viewType, provider))
 }
 
 export function deactivate() {}
+
+class ValorantSoundViewProvider implements vscode.WebviewViewProvider {
+    public static readonly viewType = 'ValorantSound.vsound'
+
+    private _view?: vscode.WebviewView
+
+    constructor(private readonly _extensionUri: vscode.Uri) {}
+
+    resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        context: vscode.WebviewViewResolveContext<unknown>,
+        token: vscode.CancellationToken
+    ): void | Thenable<void> {
+        this._view = webviewView
+
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this._extensionUri],
+        }
+
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview)
+    }
+
+    private _getHtmlForAudio(webview: vscode.Webview): string {
+        let htmlStr = ''
+        const audioFiles = ['1', '月 球 虚 度', 'Ｃａｌｌ ｍｅ', '龙卷风-周杰伦']
+        audioFiles.forEach((fileName, index) => {
+            let isChecked = ''
+            const url = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'audio', `${fileName}.mp3`))
+
+            if (index === 0) {
+                isChecked = 'checked'
+            }
+
+            htmlStr += `
+                <input type="radio" id="${index}" name="radio-group" ${isChecked}>
+                <label for="${index}">${fileName}</label>
+                <audio src="${url}" custom-id="${index}"></audio>
+            `
+        })
+
+        return htmlStr
+    }
+
+    private _getHtmlForWebview(webview: vscode.Webview) {
+        const audioHtml = this._getHtmlForAudio(webview)
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'view', 'index.js'))
+        const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'view', 'reset.css'))
+        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'view', 'index.css'))
+
+        return `<html>
+
+        <head>
+
+            <meta
+            http-equiv="Content-Security-Policy"
+            content="default-src 'none'; img-src ${webview.cspSource} https:; script-src ${webview.cspSource}; style-src ${webview.cspSource};media-src ${webview.cspSource};"
+            />
+
+            <link href="${styleResetUri}" rel="stylesheet">
+            <link href="${styleVSCodeUri}" rel="stylesheet">
+
+            <title>VSound</title>
+        </head>
+
+        <body>
+            <button>Play Sound</button>
+            <div class="radio-group">
+                ${audioHtml}
+            </div>
+
+
+            <script  src="${scriptUri}"></script>
+        </body>
+
+        </html>`
+    }
+}
---
+++ view/index.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    const audios = document.getElementsByTagName("audio")
+    console.log(audios);
+}
